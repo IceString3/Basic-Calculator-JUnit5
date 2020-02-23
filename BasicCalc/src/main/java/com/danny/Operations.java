47a0631488d7e9c77359b9f204f11d300077065c
IceString3 <sepylo@outlook.com>
2020-02-23 22:50:04 +0000

    Improved divide method

--- BasicCalc/src/main/java/com/danny/Operations.java
+++ BasicCalc/src/main/java/com/danny/Operations.java
@@ -38,7 +38,9 @@ public class Operations {
         }
     }
     public Double divide(Double n1, Double n2) throws Exception {
-        if (n2 == null || n2 <= 0) {
+        if (n1 == null || n2 == null) {
+            throw new NullPointerException ("Number cannot be null");
+        } else if (n2 <= 0) {
             throw new NumberFormatException ("Divisor cannot be less than zero or zero");
         }
         return n1 / n2;
